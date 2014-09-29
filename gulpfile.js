97083e6356b022501cb5efe39e3c0ec25181a70a
Rafael Corral <rxcorral@gmail.com>
2014-09-28 21:05:10 -0700

    Optimized images. Added gulp to do that.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    kraken = require('gulp-kraken');
+
+gulp.task('kraken', function () {
+    gulp.src('public/images/portfolio/**/*.gif')
+        .pipe(kraken({
+            key: '3364c641e11464c0480171eedf2c2cba',
+            secret: 'd33f1ca8f6d03c059156470096fde10f62b5485e',
+            lossy: true
+        }));
+});
+
+gulp.task('default', function() {
+    gulp.start('kraken');
+});
