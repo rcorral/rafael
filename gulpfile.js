96831e7b7087f48728dd1b36a3e26fc8d200866e
Rafael Corral <rxcorral@gmail.com>
2014-10-04 23:28:54 -0700

    Optimized new images.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     kraken = require('gulp-kraken');
 
 gulp.task('kraken', function () {
-    gulp.src('public/images/portfolio/**/*.gif')
+    gulp.src('public/images/**/*.*')
         .pipe(kraken({
             key: '3364c641e11464c0480171eedf2c2cba',
             secret: 'd33f1ca8f6d03c059156470096fde10f62b5485e',
